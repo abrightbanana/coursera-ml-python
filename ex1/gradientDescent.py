031d10a7a9967c355f6d6ab5705d4f6641c56800
conghuiliu <conghuiliu@upchina.com>
2022-04-13 21:55:16 +0800

    ex1

---
+++ ex1/gradientDescent.py
@@ -0,0 +1,25 @@
+import numpy as np
+from computeCost import *
+
+
+def gradient_descent(X, y, theta, alpha, num_iters):
+    # Initialize some useful values
+    m = y.size
+    J_history = np.zeros(num_iters)
+
+    for i in range(0, num_iters):
+        # ===================== Your Code Here =====================
+        # Instructions : Perform a single gradient step on the parameter vector theta
+        #
+        # Hint: X.shape = (97, 2), y.shape = (97, ), theta.shape = (2, )
+
+        error = np.dot(X, theta).flatten() - y
+        theta -= (alpha / m) * np.sum(X * error[:, np.newaxis], 0)
+
+        # ===========================================================
+        # Save the cost every iteration
+        J_history[i] = compute_cost(X, y, theta)
+
+    return theta, J_history
+
+
